4bd89f1510ce215e2bb4f7bd1c2d796fd117300d
William Rodriguez <warodriguezv@gmail.com>
2026-05-17 23:27:34 -0400

    Arreglo de test

--- app/src/main/java/com/example/registrodeocupacion/domain/empleado/usecase/EmpleadoValidation.kt
+++ app/src/main/java/com/example/registrodeocupacion/domain/empleado/usecase/EmpleadoValidation.kt
@@ -12,7 +12,7 @@ fun validarNombres(nombres: String): ValidationResult {
     return when{
         nombres.isBlank() -> ValidationResult(false, "El nombre es obligatorio")
         nombres.length < 2 -> ValidationResult(false, "Minimo 2 caracteres")
-        !nombres.all {it.isLetter()} -> ValidationResult(false, "El nombre solo debe contener letras")
+        !nombres.all {it.isLetter()||it.isWhitespace()} -> ValidationResult(false, "El nombre solo debe contener letras")
         else -> ValidationResult(true)
     }
 }
