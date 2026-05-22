bc896478e923fe71a49f0e7d14f59251d1d4e15e
William Rodriguez <warodriguezv@gmail.com>
2026-05-22 11:01:01 -0400

    arreglo de RegistroDB

--- app/src/main/java/com/example/registrodeocupacion/data/database/RegistroDB.kt
+++ app/src/main/java/com/example/registrodeocupacion/data/database/RegistroDB.kt
@@ -5,16 +5,21 @@ import androidx.room.TypeConverters
 import com.example.registrodeocupacion.data.empleado.local.Converters
 import com.example.registrodeocupacion.data.empleado.local.EmpleadoDao
 import com.example.registrodeocupacion.data.empleado.local.EmpleadoEntity
+import com.example.registrodeocupacion.data.horasextra.local.HorasExtraConverte
+import com.example.registrodeocupacion.data.horasextra.local.HorasExtraDao
+import com.example.registrodeocupacion.data.horasextra.local.HorasExtraEntity
 import com.example.registrodeocupacion.data.ocupacion.local.OcupacionDao
 import com.example.registrodeocupacion.data.ocupacion.local.OcupacioneEntity
 
 @Database(
-    entities = [OcupacioneEntity::class, EmpleadoEntity::class],
-    version = 2
+    entities = [OcupacioneEntity::class, EmpleadoEntity::class, HorasExtraEntity::class],
+    version = 3
 )
 @TypeConverters
-    (Converters::class)
+    (Converters::class, HorasExtraConverte::class)
 abstract class RegistroDB: RoomDatabase() {
     abstract fun OcupacionDao(): OcupacionDao
     abstract fun EmpleadoDao(): EmpleadoDao
+
+    abstract fun HorasExtraDao(): HorasExtraDao
 }
