6807dd51ca3c25fae30a217d8a266a2f35334713
William Rodriguez <warodriguezv@gmail.com>
2026-05-22 11:51:13 -0400

    HoraExtrasRepository domain

---
+++ app/src/main/java/com/example/registrodeocupacion/domain/horasextra/repository/HorasExtrasRepository.kt
@@ -0,0 +1,16 @@
+package com.example.registrodeocupacion.domain.horasextra.repository
+
+import com.example.registrodeocupacion.domain.horasextra.model.HoraExtra
+import kotlinx.coroutines.flow.Flow
+
+interface HorasExtrasRepository {
+    fun observeHorasExtras(): Flow<List<HoraExtra>>
+
+    suspend fun getHorasExtra(id: Int): HoraExtra?
+
+    suspend fun upsert(horaExtra: HoraExtra) : Int
+
+    suspend fun delete(id: Int)
+
+    suspend fun exists(id: Int) : Boolean
+}
